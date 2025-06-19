a45e2d82fde20a2c86166b4a99d3c5768a157943
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-06-19 16:07:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+function operations()
+{
+	const arthematicopr= new promise((resolve)=>
+		{
+			setTimeOut(()=>
+				{
+					const number=document.getElementById("ip").value;
+					document.getElemenById("output").textContent+=`result is,${number}`;
+					resolve(number);
+				}
+				,2000)
+		});
+	.then((number)=>
+		{
+			setTimeOut(()=>
+				{
+					
+					const mul =number*2;
+					document.getElemenById("output").textContent+=`result is,${mul}`;
+					resolve(mul);
+				},2000)
+		})
+	.then((number)=>
+		{
+			setTimeOut(()=>
+				{
+					
+					const sub=number-2;
+					document.getElemenById("output").textContent+=`result is,${sub}`;
+					resolve(sub);
+				})
+		})
+}
+operations();
